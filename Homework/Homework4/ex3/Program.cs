4d24d8ccf895a013848169b5235eb54027a989d9
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-29 21:11:08 +0300

    homework task with array

--- Homework/Homework4/ex3/Program.cs
+++ Homework/Homework4/ex3/Program.cs
@@ -31,12 +31,32 @@ namespace MyProgram
             PrintArray(inpLineArr);
             Console.WriteLine();
 
+            Console.WriteLine("another way of array:");
+            Console.WriteLine("input length of an array:");
+            var len = GetNumbers();
+            Console.WriteLine("Let's full the array");
+            var numbers = ManualFilling(len);
+            PrintArray(numbers);
+            Console.WriteLine();
+
         }
         static Random rd => new Random();
+        static int GetNumbers() => Convert.ToInt32(Console.ReadLine());
         static int[] Numbers() => Enumerable.Range(1, 8).Select(n => rd.Next(1, 50)).ToArray();
         static int[] NumbersNeg() => Enumerable.Range(1, 8).Select(n => rd.Next(1, 50) * (rd.Next(1, 3) == 1 ? 1 : -1)).ToArray();
         static int[] InputNumbers() => Enumerable.Range(1, 8).Select(n => Convert.ToInt32(Console.ReadLine())).ToArray();
         static int[] InputLineNumbers() => Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+        static int[] ManualFilling(int len)
+        {
+            var numbers = new int[len];
+            for (int i = 0; i<len;i++)
+            {
+                Console.Write("input {0} element :",i);
+                numbers[i] = GetNumbers();
+            }
+            return numbers;
+        }
 
         static void PrintArray(int[] numbers) => Console.WriteLine("[ " + string.Join(",", numbers.Select(n => n)) + " ]");
     }
